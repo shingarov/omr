b8b4d470fd92f268010aa7e3f899bd6b35f2c9ce
Jan Vrany <jan.vrany@fit.cvut.cz>
2019-03-14 22:16:25 +0000

    RISC-V: add instruction classes
    in separate files `RVInstruction.hpp` and `RVInstruction.cpp`.
    For now only I-type, B-type, U-type and J-type are supported.
    
    Also provide `generate*TYPE()` function to create these
    instructions. While we agreed on getting rid of this indirection,
    I found the code more concise with less 'noise".

---
+++ compiler/riscv/codegen/RVInstruction.cpp
@@ -0,0 +1,218 @@
+/*******************************************************************************
+ * Copyright (c) 2000, 2016 IBM Corp. and others
+ *
+ * This program and the accompanying materials are made available under
+ * the terms of the Eclipse Public License 2.0 which accompanies this
+ * distribution and is available at http://eclipse.org/legal/epl-2.0
+ * or the Apache License, Version 2.0 which accompanies this distribution
+ * and is available at https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the
+ * Eclipse Public License, v. 2.0 are satisfied: GNU General Public License,
+ * version 2 with the GNU Classpath Exception [1] and GNU General Public
+ * License, version 2 with the OpenJDK Assembly Exception [2].
+ *
+ * [1] https://www.gnu.org/software/classpath/license.html
+ * [2] http://openjdk.java.net/legal/assembly-exception.html
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR Apache-2.0 OR GPL-2.0 WITH Classpath-exception-2.0 OR LicenseRef-GPL-2.0 WITH Assembly-exception
+ *******************************************************************************/
+
+#include "codegen/RVInstruction.hpp"
+
+#include <stddef.h>                               // for NULL
+#include <stdint.h>                               // for int32_t, uint32_t, etc
+#include <riscv.h>
+#include "codegen/CodeGenerator.hpp"              // for CodeGenerator, etc
+#include "codegen/InstOpCode.hpp"                 // for InstOpCode, etc
+#include "codegen/Instruction.hpp"                // for Instruction
+#include "codegen/Machine.hpp"                    // for Machine, etc
+#include "codegen/MemoryReference.hpp"            // for MemoryReference
+#include "codegen/RealRegister.hpp"               // for RealRegister, etc
+#include "codegen/Register.hpp"                   // for Register
+#include "codegen/RegisterConstants.hpp"          // for TR_RegisterKinds, etc
+#include "codegen/RegisterDependency.hpp"
+#include "codegen/RegisterDependencyStruct.hpp"   // for RegisterDependency
+#include "codegen/Relocation.hpp"              // for TR::ExternalRelocation, etc
+#include "compile/Compilation.hpp"             // for Compilation
+#include "compile/ResolvedMethod.hpp"          // for TR_ResolvedMethod
+#include "control/Options.hpp"
+#include "control/Options_inlines.hpp"
+#include "env/CompilerEnv.hpp"
+#include "env/Processors.hpp"
+#include "env/TRMemory.hpp"
+#include "il/DataTypes.hpp"                       // for CONSTANT64
+#include "il/ILOps.hpp"                           // for ILOpCode
+#include "il/Node.hpp"                            // for Node
+#include "il/symbol/LabelSymbol.hpp"              // for LabelSymbol
+#include "infra/Assert.hpp"                       // for TR_ASSERT
+#include "codegen/GenerateInstructions.hpp"
+#include "codegen/ARM64OutOfLineCodeSection.hpp"
+
+#define TR_RISCV_RTYPE(insn, rd, rs1, rs2) \
+  ((insn) | ((rd) << OP_SH_RD) | ((rs1) << OP_SH_RS1) | ((rs2) << OP_SH_RS2))
+#define TR_RISCV_ITYPE(insn, rd, rs1, imm) \
+  ((insn) | ((rd) << OP_SH_RD) | ((rs1) << OP_SH_RS1) | ENCODE_ITYPE_IMM(imm))
+#define TR_RISCV_STYPE(insn, rs1, rs2, imm) \
+  ((insn) | ((rs1) << OP_SH_RS1) | ((rs2) << OP_SH_RS2) | ENCODE_STYPE_IMM(imm))
+#define TR_RISCV_SBTYPE(insn, rs1, rs2, target) \
+  ((insn) | ((rs1) << OP_SH_RS1) | ((rs2) << OP_SH_RS2) | ENCODE_SBTYPE_IMM(target))
+#define TR_RISCV_UTYPE(insn, rd, bigimm) \
+  ((insn) | ((rd) << OP_SH_RD) | ENCODE_UTYPE_IMM(bigimm))
+#define TR_RISCV_UJTYPE(insn, rd, target) \
+  ((insn) | ((rd) << OP_SH_RD) | ENCODE_UJTYPE_IMM(target))
+
+
+uint8_t *TR::ItypeInstruction::generateBinaryEncoding() {
+   uint8_t        *instructionStart = cg()->getBinaryBufferCursor();
+   uint8_t        *cursor           = instructionStart;
+   uint32_t *iPtr = (uint32_t*)instructionStart;
+
+   *iPtr = TR_RISCV_ITYPE ((uint32_t)(getOpCode().getOpCodeBinaryEncoding()),
+                         toRealRegister(getTargetRegister())->binaryRegCode(),
+                         toRealRegister(getSource1Register())->binaryRegCode(),
+                         getSourceImmediate());
+
+   cursor += RISCV_INSTRUCTION_LENGTH;
+   setBinaryLength(RISCV_INSTRUCTION_LENGTH);
+   setBinaryEncoding(instructionStart);
+   return cursor;
+}
+
+
+uint8_t *TR::BtypeInstruction::generateBinaryEncoding()
+   {
+   uint8_t        *instructionStart = cg()->getBinaryBufferCursor();
+   uint8_t        *cursor           = instructionStart;
+   uint32_t *iPtr = (uint32_t*)instructionStart;
+
+   TR::LabelSymbol *label            = getLabelSymbol();
+
+
+   *iPtr = TR_RISCV_SBTYPE ((uint32_t)(getOpCode().getOpCodeBinaryEncoding()),
+                         toRealRegister(_src1)->binaryRegCode(),
+                         toRealRegister(_src2)->binaryRegCode(),
+                         0 /* to fix up in the future */ );
+
+   if (label->getCodeLocation() != NULL) {
+           // we already know the address
+           int32_t delta = label->getCodeLocation() - cursor;
+           *iPtr |= ENCODE_SBTYPE_IMM(delta);
+   } else {
+           cg()->addRelocation(new (cg()->trHeapMemory()) TR::LabelRelative16BitRelocation(cursor, label));
+   }
+
+   cursor += RISCV_INSTRUCTION_LENGTH;
+   setBinaryLength(RISCV_INSTRUCTION_LENGTH);
+   setBinaryEncoding(instructionStart);
+   return cursor;
+   }
+
+uint8_t *TR::UtypeInstruction::generateBinaryEncoding() {
+   uint8_t        *instructionStart = cg()->getBinaryBufferCursor();
+   uint8_t        *cursor           = instructionStart;
+   uint32_t *iPtr = (uint32_t*)instructionStart;
+
+   *iPtr = TR_RISCV_UTYPE ((uint32_t)(getOpCode().getOpCodeBinaryEncoding()),
+                         toRealRegister(getTargetRegister())->binaryRegCode(),
+                         _imm);
+
+   cursor += RISCV_INSTRUCTION_LENGTH;
+   setBinaryLength(RISCV_INSTRUCTION_LENGTH);
+   setBinaryEncoding(instructionStart);
+   return cursor;
+}
+
+uint8_t *TR::JtypeInstruction::generateBinaryEncoding() {
+   uint8_t        *instructionStart = cg()->getBinaryBufferCursor();
+   uint8_t        *cursor           = instructionStart;
+   uint32_t *iPtr = (uint32_t*)instructionStart;
+   ptrdiff_t offset = 0;
+
+   // First, compute the value of _imm
+   {
+      TR::ResolvedMethodSymbol *sym = getSymbolReference()->getSymbol()->getResolvedMethodSymbol();
+      TR_ResolvedMethod *resolvedMethod = sym == NULL ? NULL : sym->getResolvedMethod();
+
+      if (resolvedMethod != NULL && resolvedMethod->isSameMethod(cg()->comp()->getCurrentMethod()))
+         {
+         offset = cg()->getCodeStart() - cursor;
+         // to guard against silent truncation
+         TR_ASSERT( (INT32_MIN <= offset) && (offset <= INT32_MAX), "Jump offset out of range" );
+         }
+      else
+         {
+         TR_ASSERT(0, "Non-recursive calls not (yet) supported");
+         }
+   }
+
+
+   TR_ASSERT(VALID_UJTYPE_IMM(offset), "Jump offset out of range");
+   *iPtr = TR_RISCV_UJTYPE ((uint32_t)(getOpCode().getOpCodeBinaryEncoding()),
+                         toRealRegister(getTargetRegister(0))->binaryRegCode(),
+                         offset);
+
+   cursor += RISCV_INSTRUCTION_LENGTH;
+   setBinaryLength(RISCV_INSTRUCTION_LENGTH);
+   setBinaryEncoding(instructionStart);
+   return cursor;
+
+}
+
+TR::Instruction *generateITYPE( TR::InstOpCode::Mnemonic op,
+                                TR::Node          *n,
+                                TR::Register      *treg,
+                                TR::Register      *sreg,
+                                uint32_t          imm,
+                                TR::CodeGenerator *cg,
+                                TR::Instruction   *previous)
+   {
+   if (previous)
+      return new (cg->trHeapMemory()) TR::ItypeInstruction(op, n, treg, sreg, imm, previous, cg);
+   else
+      return new (cg->trHeapMemory()) TR::ItypeInstruction(op, n, treg, sreg, imm, cg);
+   }
+
+TR::Instruction *generateBTYPE( TR::InstOpCode::Mnemonic op,
+                                TR::Node          *n,
+                                TR::LabelSymbol   *sym,
+                                TR::Register      *src1,
+                                TR::Register      *src2,
+                                TR::CodeGenerator *cg,
+                                TR::Instruction   *previous)
+   {
+   if (previous)
+      return new (cg->trHeapMemory()) TR::BtypeInstruction(op, n, sym, src1, src2, previous, cg);
+   else
+      return new (cg->trHeapMemory()) TR::BtypeInstruction(op, n, sym, src1, src2, cg);
+   }
+
+TR::Instruction *generateUTYPE( TR::InstOpCode::Mnemonic op,
+                                TR::Node          *n,
+                                uint32_t          imm,
+                                TR::Register      *reg,
+                                TR::CodeGenerator *cg,
+                                TR::Instruction   *previous)
+   {
+   if (previous)
+      return new (cg->trHeapMemory()) TR::UtypeInstruction(op, n, imm, reg, previous, cg);
+   else
+      return new (cg->trHeapMemory()) TR::UtypeInstruction(op, n, imm, reg, cg);
+   }
+
+TR::Instruction *generateJTYPE( TR::InstOpCode::Mnemonic op,
+                                TR::Node          *n,
+                                TR::Register      *trgReg,
+                                uintptr_t        imm,
+                                TR::RegisterDependencyConditions *cond,
+                                TR::SymbolReference *sr,
+                                TR::Snippet       *s,
+                                TR::CodeGenerator *cg,
+                                TR::Instruction   *previous)
+   {
+   if (previous)
+      return new (cg->trHeapMemory()) TR::JtypeInstruction(op, n, trgReg, imm, cond, sr, s, previous, cg);
+   else
+      return new (cg->trHeapMemory()) TR::JtypeInstruction(op, n, trgReg, imm, cond, sr, s, cg);
+   }
---
+++ compiler/riscv/codegen/RVInstruction.hpp
@@ -0,0 +1,216 @@
+/*******************************************************************************
+ * Copyright (c) 2000, 2019 Boris Shingarov, Jan Vrany
+ *
+ * This program and the accompanying materials are made available under
+ * the terms of the Eclipse Public License 2.0 which accompanies this
+ * distribution and is available at http://eclipse.org/legal/epl-2.0
+ * or the Apache License, Version 2.0 which accompanies this distribution
+ * and is available at https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the
+ * Eclipse Public License, v. 2.0 are satisfied: GNU General Public License,
+ * version 2 with the GNU Classpath Exception [1] and GNU General Public
+ * License, version 2 with the OpenJDK Assembly Exception [2].
+ *
+ * [1] https://www.gnu.org/software/classpath/license.html
+ * [2] http://openjdk.java.net/legal/assembly-exception.html
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR Apache-2.0 OR GPL-2.0 WITH Classpath-exception-2.0 OR LicenseRef-GPL-2.0 WITH Assembly-exception
+ *******************************************************************************/
+
+#ifndef RVINSTRUCTION_INCL
+#define RVINSTRUCTION_INCL
+
+#include "codegen/ARM64Instruction.hpp"
+
+#define RISCV_INSTRUCTION_LENGTH 4
+
+namespace TR
+{
+
+class ItypeInstruction : public ARM64Trg1Src1Instruction
+   {
+
+   uint32_t _imm;
+
+   public:
+
+   ItypeInstruction(TR::InstOpCode::Mnemonic op,
+         TR::Node          *n,
+         TR::Register      *treg,
+         TR::Register      *sreg,
+         uint32_t          imm,
+         TR::CodeGenerator *codeGen)
+
+      : ARM64Trg1Src1Instruction(op, n, treg, sreg, codeGen),
+        _imm(imm)
+      {
+      }
+
+   ItypeInstruction(TR::InstOpCode::Mnemonic op,
+         TR::Node          *n,
+         TR::Register      *treg,
+         TR::Register      *sreg,
+         uint32_t          imm,
+         TR::Instruction   *precedingInstruction,
+         TR::CodeGenerator *codeGen)
+      : ARM64Trg1Src1Instruction(op, n, treg, sreg, precedingInstruction, codeGen),
+        _imm(imm)
+      {
+      }
+
+   uint32_t getSourceImmediate()            {return _imm;}
+   uint32_t setSourceImmediate(uint32_t si) {return (_imm = si);}
+
+   virtual uint8_t *generateBinaryEncoding();
+
+   };
+
+class BtypeInstruction : public ARM64LabelInstruction
+   {
+   protected:
+
+   TR::Register    *_src1;
+   TR::Register    *_src2;
+
+   public:
+     BtypeInstruction(
+       TR::InstOpCode::Mnemonic op,
+       TR::Node                 *n,
+       TR::LabelSymbol          *sym,
+       TR::Register             *src1,
+       TR::Register             *src2,
+       TR::CodeGenerator        *cg
+       )
+     : ARM64LabelInstruction(op, n, sym, cg), _src1(src1), _src2(src2)
+     {
+     useRegister(_src1);
+     useRegister(_src2);
+     }
+
+     BtypeInstruction(
+       TR::InstOpCode::Mnemonic op,
+       TR::Node                 *n,
+       TR::LabelSymbol          *sym,
+       TR::Register             *src1,
+       TR::Register             *src2,
+       TR::Instruction          *precedingInstruction,
+       TR::CodeGenerator        *cg
+       )
+     : ARM64LabelInstruction(op, n, sym, precedingInstruction, cg), _src1(src1), _src2(src2)
+     {
+     useRegister(_src1);
+     useRegister(_src2);
+     }
+
+   virtual uint8_t *generateBinaryEncoding();
+   };
+
+class UtypeInstruction : public ARM64Trg1Instruction
+   {
+   protected:
+
+   uint32_t _imm;
+
+   public:
+
+   UtypeInstruction(TR::InstOpCode::Mnemonic op,
+         TR::Node          *n,
+         uint32_t          imm,
+         TR::Register      *reg,
+         TR::CodeGenerator *codeGen)
+      : ARM64Trg1Instruction(op, n, reg, codeGen), _imm(imm)
+      {
+      }
+
+   UtypeInstruction(TR::InstOpCode::Mnemonic op,
+         TR::Node          *n,
+         uint32_t          imm,
+         TR::Register      *reg,
+         TR::Instruction   *precedingInstruction,
+         TR::CodeGenerator *codeGen)
+      : ARM64Trg1Instruction(op, n, reg, precedingInstruction, codeGen), _imm(imm)
+      {
+      }
+
+   virtual uint8_t *generateBinaryEncoding();
+   };
+
+class JtypeInstruction : public ARM64ImmSymInstruction
+   {
+   TR::Register *_target1Register;
+
+   public:
+
+   JtypeInstruction(TR::InstOpCode::Mnemonic op,
+         TR::Node          *n,
+         TR::Register      *trgReg,
+         uintptr_t        imm,
+         TR::RegisterDependencyConditions *cond,
+         TR::SymbolReference *sr,
+         TR::Snippet       *s,
+         TR::CodeGenerator *codeGen)
+
+      : ARM64ImmSymInstruction(op, n, imm, cond, sr, s, codeGen),
+        _target1Register(trgReg)
+      {
+      }
+
+   JtypeInstruction(TR::InstOpCode::Mnemonic op,
+         TR::Node          *n,
+         TR::Register      *trgReg,
+         uintptr_t        imm,
+         TR::RegisterDependencyConditions *cond,
+         TR::SymbolReference *sr,
+         TR::Snippet       *s,
+         TR::Instruction   *precedingInstruction,
+         TR::CodeGenerator *codeGen)
+      : ARM64ImmSymInstruction(op, n, imm, cond, sr, s, precedingInstruction, codeGen),
+        _target1Register(trgReg)
+      {
+      }
+
+   TR::Register *getTargetRegister()               {return _target1Register;}
+   TR::Register *setTargetRegister(TR::Register *r) {return (_target1Register = r);}
+   virtual TR::Register *getTargetRegister(uint32_t i)       {if (i==0) return _target1Register; return NULL;}
+
+   virtual uint8_t *generateBinaryEncoding();
+   };
+
+} // namespace TR
+
+TR::Instruction *generateITYPE( TR::InstOpCode::Mnemonic op,
+                                TR::Node          *n,
+                                TR::Register      *treg,
+                                TR::Register      *sreg,
+                                uint32_t          imm,
+                                TR::CodeGenerator *cg,
+                                TR::Instruction   *previous = NULL);
+
+TR::Instruction *generateBTYPE( TR::InstOpCode::Mnemonic op,
+                                TR::Node          *n,
+                                TR::LabelSymbol   *sym,
+                                TR::Register      *src1,
+                                TR::Register      *src2,
+                                TR::CodeGenerator *cg,
+                                TR::Instruction   *previous = NULL);
+
+TR::Instruction *generateUTYPE( TR::InstOpCode::Mnemonic op,
+                                TR::Node          *n,
+                                uint32_t          imm,
+                                TR::Register      *reg,
+                                TR::CodeGenerator *cg,
+                                TR::Instruction   *previous = NULL);
+
+TR::Instruction *generateJTYPE( TR::InstOpCode::Mnemonic op,
+                                TR::Node          *n,
+                                TR::Register      *trgReg,
+                                uintptr_t        imm,
+                                TR::RegisterDependencyConditions *cond,
+                                TR::SymbolReference *sr,
+                                TR::Snippet       *s,
+                                TR::CodeGenerator *cg,
+                                TR::Instruction   *previous = NULL);
+
+#endif // RVINSTRUCTION_INCL
